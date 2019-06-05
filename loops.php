16782027b062258e928be7063349761f4d869ce2
TiffanyClarke <50335667+TiffanyClarke@users.noreply.github.com>
2019-06-04 20:25:27 -0400

    Loops
    ...I tired. I definitely need more practice.

---
+++ loops.php
@@ -0,0 +1,182 @@
+<?php
+/*
++---+
+| 1 |
++---+
+Use while-loop to print the numbers 1 to 10.
+*/
+
+$count = 1;
+    while ($count <= 10) {
+        echo $count. "<br>";
+        $count++;
+    };
+
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/*
++---+
+| 2 |
++---+
+Use the PHP function range(start, end, step) 
+to create array of numbers 5 to 100 with the step 5 
+(example: 5, 10, 15, ...)
+*/
+$fives = [
+    5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60,
+    65, 70, 75, 80, 85, 90, 95, 100];
+/*
+Use for-loop and if-statement to print all odd numbers.
+
+How to check if a number is odd or even? 
+Divide it by 2, and if there is a remainder, the number is odd. 
+Use modulus operator (outputs the remainder after division), 
+for example: 
+1 % 2 = 1 -> odd number
+2 % 2 = 0 -> even number
+3 % 2 = 1 -> odd number
+4 % 2 = 0 -> even number
+*/
+
+//Syntax error that I can't figure out... at least I tried
+/*foreach($fives) {
+    if (1 % 2 = 1) {
+    echo "The Number is odd"
+    };
+};*/
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/*
++---+
+| 3 |
++---+
+Declare and assign the indexed array with your favourite food 
+(at least 4 array elements). Name the array food.
+*/
+$food = [
+     "Schwarma",
+     "Cheesecake",
+     "Pho",
+     "Eggs",
+     "Pizza"
+ ];
+/*
+Use while-loop to print array elements (every food in a new row).
+*/
+$x=0;
+ 
+    while ($x < sizeof($food)) {
+        echo "<p> $food[$x] </p>";
+        $x++;
+};
+/*
+Use for-loop to print array elements (every food in a new row).
+*/
+
+/*Unexpected { on line 75. Why?
+for ($x = 0; $x < sizeof($food); $x++) {
+        echo {$food[$x]}."<br>";
+};*/
+
+/*
+Use foreach-loop to print array elements (every food in a new row).
+*/
+foreach ($food as $k => $v) {
+    echo "{$v} <br>";
+};
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/*
++---+
+| 4 |
++---+
+Print the array from task 3 as unordered list. Printing has to be done inside the for-loop.
+
+Before looping, you need to print the opening list-tag <ul>
+After looping, you need to print the closing list-tag </ul>
+*/ 
+echo "<ul>";
+    $x = 0;
+    while ($x < sizeof($food)){
+        echo "<li>$food[$x]</li>";
+        $x++;
+    }
+    echo "</ul>";
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/*
++---+
+| 5 |
++---+
+The array from task 3 is turned into associative array. 
+The meal courses are broken down based on the type (salad, main course, dessert, ...) 
+and country of origin (Italy, Spain, India, ...)
+
+$food_assoc = [
+  "pizza" => [
+    "type" => "main course",
+    "origin" => "Italy"
+  ],
+  "cheesesake" => [
+    "type" => "desert",
+    "origin" => "Greece"
+  ]
+]
+
+So, food_assoc is an associative array. 
+Every key of food_assoc (pizza, cheesecake) will now have the value that is associative array itself 
+and carries the information about the type and origin.
+*/
+$food_assoc = [
+     "Schwarma" => [
+         "type" => "Main Course",
+         "Origin" => "Ottoman Empire"
+     ],
+     "Cheesecake" => [
+         "type" => "Desert",
+         "Origin" => "Greece"
+     ],
+     "Pho" => [
+         "type" => "Soup",
+         "Origin" => "Vietnam"
+     ],
+     "Eggs" => [
+        "type" => "Main Side",
+         "Origin" => "Chicken" 
+     ],
+     "Pizza" => [
+         "type" => "Main Course",
+         "Origin" => "Italy"
+     ]
+ ];
+/*
+Loop through $food_assoc and use print_r() to print the entire meal course 
+(entire array that includes type and origin).
+*/
+
+echo "<pre>";
+print_r($food_assoc);
+echo "</pre>";
+
+/*
+Loop through $food_assoc and print the meal names (keys of $food_assoc)
+as unordered list-items. Loop through the current meal (inner loop through sub-array) 
+and print the type and origin as nested list items (see the example below).
+*/
+
+//Not sure why this isn't working
+/*echo"<ul>"
+foreach ($food_assoc as $type => $origin);
+    echo "<li>" $name. ',' .$origin  "</li>"
+echo"</ul>"*/
+
+    
+?>
